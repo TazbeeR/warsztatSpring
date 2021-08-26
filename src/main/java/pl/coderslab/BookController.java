f5ac95a7e64e9a0edf680abb176d61cdaa50f502
TazbeeR <myszkiewicz.robert81@gmail.com>
2021-08-26 23:08:49 +0200

    addBook.jsp added

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -1,9 +1,6 @@
 package pl.coderslab;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,9 +19,10 @@ public Book helloBook() {
     return new Book(1L, "9788324631766", "Thinking in Java",
             "Bruce Eckel", "Helion", "programming");
     }
-    @PostMapping("/books")
+    @PostMapping("/addbook")
     public void addBook(@RequestBody Book book){
-        book
+        bookList.add(book);
+//        @ResponseBody
     }
 
 }
